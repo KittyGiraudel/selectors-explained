9eb0a8170a55b0abd25112162f9b91c833c0ac99
Hugo Giraudel <hugo.giraudel@gmail.com>
2020-02-23 19:17:12 +0100

    Apply minor code improvement

--- src/helpers/getSelectorDetails/index.js
+++ src/helpers/getSelectorDetails/index.js
@@ -2,17 +2,13 @@ import parseAttributes from '../parseAttributes'
 import parseClasses from '../parseClasses'
 import parseId from '../parseId'
 
-const _with = items => {
-  return items.reduce((acc, item, index) => {
-    if (index === 0) return acc + 'with ' + item
-    else return acc + ' and ' + item
-  }, '')
-}
-
 export default selector => {
   const components = [parseId, parseClasses, parseAttributes]
     .map(fn => fn(selector))
     .filter(Boolean)
 
-  return _with(components)
+  return components.reduce((acc, item, index) => {
+    if (index === 0) return acc + 'with ' + item
+    else return acc + ' and ' + item
+  }, '')
 }
